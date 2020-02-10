e47b79c750c09f67e5c7320bb8243daddc63b1f7
Haag-Martha <haa18001@byui.edu>
2020-02-10 16:27:49 -0500

    local storage

---
+++ scripts/local.js
@@ -0,0 +1,46 @@
+var myList = document.getElementById("myList");
+var txtTodo = document.getElementById("txtTodo");
+var toDoList = [];
+
+function removeItem(itemToRemove){  
+  myList.removeChild(itemToRemove);
+  
+  var positionInArray = toDoList.indexOf(itemToRemove.innerText);
+  toDoList.splice(positionInArray, 1);
+  
+  
+  
+  localStorage.setItem('toDList', JSON.stringify(toDoList));
+}
+
+function addItem(text){
+  var newLiChild = document.createElement("li"); 
+  
+  if(text == undefined) {
+    newLiChild.innerText = txtTodo.value;
+    toDoList.push(newLiChild.innerText);
+  }
+  else
+    newLiChild.innerText = text;
+  
+  newLiChild.onclick = function(){ removeItem(this); };
+  myList.appendChild(newLiChild);
+  
+  txtTodo.value = "";  
+  
+  localStorage.setItem('toDoList', JSON.stringify(toDoList));
+}
+
+function loadSavedItems(){
+      var JSONObject = localStorage.getItem("toDoList");
+    if(JSONObject != undefined){
+      toDoList = JSON.parse(JSONObject);
+      
+      for(var i = 0; i <= toDoList.length - 1; i++){
+        addItem(toDoList[i]);
+      }
+    }
+}
+
+loadSavedItems();
+
